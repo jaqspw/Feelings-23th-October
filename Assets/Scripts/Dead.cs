cfdc411db2b560f312161c022f6a6693b8a22116
Jaqueline Santos <jaqspw@gmail.com>
2017-10-29 17:43:46 -0200

    29th October
    Adicionados os NPCs, configurações de build.

---
+++ Assets/Scripts/Dead.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dead : MonoBehaviour {
+
+
+	private Scene scene;
+
+	// Use this for initialization
+	void Start () {
+		scene = SceneManager.GetActiveScene ();
+	}
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			Application.LoadLevel (scene.name);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
